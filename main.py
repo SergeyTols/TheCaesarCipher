bff15ba69f4177b1a67e26064b461687c5caaf52
Sarge <sarge2033@gmail.com>
2025-06-29 21:12:53 +0300

    En, De, choice

--- main.py
+++ main.py
@@ -1,22 +1,29 @@
 abc = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
-word = 'абстрактно'
-# word = 'гдфхугнхрсваяб'
-step = 3
-new_word = ''
-g = ''
-n = 33
 
-for letter in word:
-        if letter in abc:
-            i = abc.index(letter)
-            new_letter = (i + step) % 33
-            new_word += abc[new_letter]
-print('Результат: ' + new_word)
+while True:
+    print('Введите E чтобы зашифровать сообщение, D чтобы расшифровать и Q чтобы выйти')
+    choice = input('>>> ').lower()
+    if choice == 'q':
+        break
+    elif not (choice == 'e' or choice == 'd'):
+        continue
+    new_word = ''
+    word = input('Введите слово: ').lower()
+    step = int(input('Введите шаг: '))
+    if choice == 'd':
+        step *= -1
+    for letter in word:
+            if letter in abc:
+                i = abc.index(letter)
+                new_letter = (i + step) % 33
+                new_word += abc[new_letter]
+    print('Результат: ' + new_word)
 
-print((0 + 3) % 33)
+
 
 # гдфхугнхрс
 
+# йстг
 
 
 
