6537c303c034df5fcf1783fe0ac23f6c77c1cca4
Sarge <a@b.ru>
2025-06-30 10:36:26 +0300

    Требует доработки

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 abc = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
-
+#  abc += abc.upper()
+#  abc_u = abc.upper()
 while True:
     print('Введите E - чтобы зашифровать сообщение, D - чтобы расшифровать или Q -  чтобы выйти')
     choice = input('>>> ').lower()
@@ -9,15 +10,17 @@ while True:
         continue
     new_word = ''
     word = input('Введите слово: ').lower()
+    #  word = input('Введите слово: ')
     step = int(input('Введите шаг: '))
     if choice == 'd':
         step *= -1
     for letter in word:
         if letter in abc:
             i = abc.index(letter)
-            new_letter = (i + step) % 33
+            new_letter = (i + step) % len(abc)
             new_word += abc[new_letter]
     print('Результат: ' + new_word)
+
 
 
 
